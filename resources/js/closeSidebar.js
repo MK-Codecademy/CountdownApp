5c42dec280e037908842b10c7ac40080df4debe7
Will Garrett <willgarrett64@yahoo.com>
2021-10-23 22:32:19 +0100

    small fix - removed semi-colon that was in wrong place

--- resources/js/closeSidebar.js
+++ resources/js/closeSidebar.js
@@ -1,6 +1,6 @@
 const openAndCloseSidebar = () => {
-  document.getElementById('sidebar');.classList.toggle("open");
-  document.getElementById('main');.classList.toggle("open");
+  document.getElementById('sidebar').classList.toggle("open");
+  document.getElementById('main').classList.toggle("open");
 }
 
 
