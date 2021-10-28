b7888f433e0da8631ab838e13872f6d57c323631
Will Garrett <willgarrett64@yahoo.com>
2021-10-28 18:01:02 +0100

    remove module because of CORS issue

--- resources/js/createCountdownItem.js
+++ resources/js/createCountdownItem.js
@@ -1,4 +1,20 @@
-import { createElement } from "./modules/createElement.js";
+// a function to speed up the creation of HTML elements through JS. Only the elementType is mandatory. 
+// All parameters are strings, for example:
+// elementType = 'div'
+// classes = 'button red extra-wide'
+// id = 'red-button'
+const createElement = (elementType, classes, id) => {
+  const element = document.createElement(elementType);
+  
+  if (classes) {
+    element.className = classes;
+  } 
+  if (id) {
+    element.id = id;
+  }
+
+  return element;
+};
 
 
 // Each countdown card displays the name, date and time info. This function creates each individual info section
